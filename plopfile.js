10d6b7cacb9c6899be0a4c78c014fa6298edf586
nisheed.jagadish <nisheed.jagadish@loblaw.ca>
2021-05-03 05:28:41 -0400

    Added plop gen

---
+++ plopfile.js
@@ -0,0 +1,32 @@
+module.exports = function (plop) {
+    // create your generators here
+    plop.setGenerator('blocks', {
+        description: 'Generator for blockly blocks in playwright',
+        prompts: [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Block name:',
+            },
+            {
+                type: 'list',
+                name: 'type',
+                message: 'Type:',
+                choices: ['assertions', 'browser', 'steps'],
+                default: 0,
+            },
+        ], // array of inquirer prompts
+        actions: [
+            {
+                type: 'add',
+                path: 'src/blocks/{{type}}/{{camelCase name}}/definition.json',
+                template: '{}',
+            },
+            {
+                type: 'add',
+                path: 'src/blocks/{{type}}/{{camelCase name}}/index.js',
+                templateFile: 'templates/block.hbs',
+            },
+        ], // array of actions
+    });
+};
