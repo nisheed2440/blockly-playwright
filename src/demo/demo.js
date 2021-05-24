0ec4a7d6074d3624620437fa0a4dad1477c10513
Nisheed Jagadish <njagadish@Nisheeds-MacBook-Pro.local>
2021-05-24 11:30:48 +0530

    Added localstorage code for saved workspace

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -8,6 +8,9 @@ if (window.Worker) {
     // Setup variables
     let copyText = '';
     let demoWorkspace = null;
+    // Get data from localstorage if it exists
+    const inputXML = localStorage.getItem('workspace');
+    // Toastr options
     window.toastr.options.positionClass = 'toast-bottom-right';
     window.toastr.options.preventDuplicates = true;
     window.toastr.options.timeOut = 500;
@@ -57,8 +60,21 @@ if (window.Worker) {
     // Add listener for workspace changes and send it to webworker
     demoWorkspace.addChangeListener(() => {
         const output = Blockly.JavaScript.workspaceToCode(demoWorkspace);
+        const outputXML = Blockly.Xml.domToText(
+            Blockly.Xml.workspaceToDom(demoWorkspace)
+        );
         worker.postMessage(output);
+        // Update the localstorage with the latest updates to workspace
+        localStorage.setItem('workspace', outputXML);
     });
+
+    // If there exists a saved workspace initialize the demo with it
+    if (inputXML) {
+        Blockly.Xml.domToWorkspace(
+            Blockly.Xml.textToDom(inputXML),
+            demoWorkspace
+        );
+    }
 } else {
     console.error(
         'Web Workers not supported!. Please use this application on Chrome/Firefox/MS Edge'
